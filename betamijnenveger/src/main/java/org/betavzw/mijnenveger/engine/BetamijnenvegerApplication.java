bed0e25b26e4d57a28238948ba94ce97db834eb0
Jef De Smedt <Jef.DeSmedt@betavzw.org>
2019-01-02 06:59:02 +0100

    begin

---
+++ betamijnenveger/src/main/java/org/betavzw/mijnenveger/engine/BetamijnenvegerApplication.java
@@ -0,0 +1,14 @@
+package org.betavzw.mijnenveger.engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetamijnenvegerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetamijnenvegerApplication.class, args);
+	}
+
+}
+
